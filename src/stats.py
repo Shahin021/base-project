39f289911e69dd4c9c86af9017584a7ccf6a3e44
shahin021 <shahin.l20uh@gmail.com>
2026-06-25 19:02:11 +0330

    Add tag statistics helper

--- src/stats.py
+++ src/stats.py
@@ -9,3 +9,13 @@ def count_by_category(tracker: ProjectTracker):
 
 def count_by_status(tracker: ProjectTracker):
     return Counter(project.status for project in tracker.list_projects())
+
+
+def count_by_tag(tracker: ProjectTracker):
+    counter = Counter()
+
+    for project in tracker.list_projects():
+        for tag in project.tags:
+            counter[tag] += 1
+
+    return counter
